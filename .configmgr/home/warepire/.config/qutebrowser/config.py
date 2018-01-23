f9ea1314138076f8a969af92c774d0936e18a141
Warepire <Warepire@users.noreply.github.com>
2018-01-23 23:02:39 +0100

    qutebrowser config

---
+++ .configmgr/home/warepire/.config/qutebrowser/config.py
@@ -0,0 +1,4 @@
+c.qt.force_software_rendering = True
+c.new_instance_open_target = "tab-bg-silent"
+c.spellcheck.languages = [ "en-US" ]
+c.url.searchengines = { "DEFAULT": "https://www.google.com/search?q={}", "g": "https://www.google.com/search?q={}" }
